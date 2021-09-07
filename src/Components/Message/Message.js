d6cad711f9b1e2c6856e7d3cb8412f000b753793
C-Max <ksr-kursk@yandex.ru>
2021-09-07 15:57:08 +0300

    HomeWork 1

---
+++ src/Components/Message/Message.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Message = ({ firstProp }) => {
+    return (
+        <div className="firstStyle">
+            { firstProp }
+        </div>
+    )
+};
+
+export default Message;
